a8660910dbf6f89be2f61ef040c539dea10e435a
Sample123 <119918517+kou4869@users.noreply.github.com>
2023-07-09 18:56:15 +0900

    tuika

---
+++ lesson01/sample02.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>sample01</title>
+</head>
+
+<body>
+
+  <?php
+    echo "I'm studying";
+    echo 'I\'m studying';
+    // ↑のように「'」を表示させたりしたい場合は「\」を付け足して『エスケープシーケンス』として設置する
+  ?>
+
+</body>
+
+</html>
